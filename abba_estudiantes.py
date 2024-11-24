472e35ca762e177549f43368cc54df5c3d5103fb
Juan Manuel Saad <juanmanuel.saad@hotmail.com.ar>
2024-11-23 22:55:25 -0300

    implementacion base del grafo

--- abba_estudiantes.py
+++ abba_estudiantes.py
@@ -1,33 +1,76 @@
 from itertools import permutations, product
 from collections import deque
+from typing import Any, Dict, List, Set
 
+class Vertice:
+    def __init__(self, nombre: str) -> None:
+        self.nombre = nombre
 
-def es_palindromo(s: str) -> bool:
-    """
-    Determina si una cadena es un palíndromo. Una cadena es palíndromo si se lee
-    igual de izquierda a derecha y de derecha a izquierda.
-    """
-    # Completar
-    ...
+    def get_nombre(self) -> str:
+        return self.nombre
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+    def __repr__(self) -> str:
+        return f"{self.nombre
+    }"
 
 
 class GrafoDirigido:
     def __init__(self) -> None:
-        # Completar
-        ...
+        self.vertices: List[Vertice] = []
+        self.vecinos: Dict[Vertice, Set[Vertice]] = {}
 
-    def agregar_vertice(self, vertice) -> None:
-        # Comletar
-        ...
+    def agregar_vertice(self, vertice: "Vertice") -> None:
+        if vertice not in self.vecinos:
+            self.vertices.append(vertice)
+            self.vecinos[vertice] = set()
 
-    def agregar_arista(self, origen, destino) -> None:
-        # Completar
-        ...
+    def agregar_arista(self, origen: "Vertice", destino: "Vertice") -> None:
+        if origen not in self.vecinos:
+            self.agregar_vertice(origen)
+
+        if destino not in self.vecinos:
+            self.agregar_vertice(destino)
+
+        self.vecinos[origen].add(destino)
+
+    def get_adjacent(self, vertice: "Vertice") -> Any:
+        return self.vecinos[vertice]
+
+    def get_nodes(self) -> list["Vertice"]:
+        return self.vertices
 
     def __eq__(self, other: "GrafoDirigido") -> bool:
         """compara dos grafos dirigidos (sin tener en cuenta el orden de los conjuntos de vertices y aristas)"""
-        # Completar
-        ...
+
+        if len(self.vecinos.keys()) != len(other.vecinos.keys()):
+            return False
+
+        for vertice, vecinos in self.vecinos.items():
+            if vertice not in other.vecinos:
+                return False
+
+            if vecinos != other.vecinos[vertice]:
+                return False
+
+        return True
+
+    def __str__(self) -> str:
+        grafo_str = "{"
+        for vertice, vecinos in self.vecinos.items():
+            grafo_str += (
+                f" {vertice.get_nombre()}: {{"
+                + ", ".join([str(v.get_nombre()) for v in vecinos])
+                + "}},"
+            )
+        grafo_str = grafo_str.rstrip(",") + " }"
+        return grafo_str
+
+
+def es_palindromo(s: str) -> bool:
+    return s == s[::-1]
 
 
 def generar_G_r(n: int, alfabeto: list[str]) -> GrafoDirigido | None:
@@ -62,7 +105,22 @@ def distancia_a_palindromo(grafo: GrafoDirigido, start: str) -> int:
     ...
 
 
+# Ejemplo basico de uso
+grafo_basico = GrafoDirigido()
+v1 = Vertice(1)
+v2 = Vertice(2)
+v3 = Vertice(3)
+
+grafo_basico.agregar_vertice(v1)
+grafo_basico.agregar_vertice(v2)
+grafo_basico.agregar_vertice(v3)
+
+grafo_basico.agregar_arista(v1, v2)
+grafo_basico.agregar_arista(v1, v3)
+
+print(grafo_basico)
+
 # Ejemplo Básico:
-grafo = generar_G_r(4, ["o", "n", "c", "e"])
-print(grafo.vecinos)
-print(distancia_a_palindromo(grafo, "once"))  # Deberia devolver 2.
+# grafo = generar_G_r(4, ["o", "n", "c", "e"])
+# print(grafo.vecinos)
+# print(distancia_a_palindromo(grafo, "once"))  # Deberia devolver 2.
